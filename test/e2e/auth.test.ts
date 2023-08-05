c346d12b8dd667fefbba94816f03e0a3f1871e3a
Treasure <obisiket@yahoo.com>
2023-08-05 21:51:02 +0100

    chore: git added test and documenter

---
+++ test/e2e/auth.test.ts
@@ -0,0 +1,27 @@
+import { assert } from 'chai';
+import supertest from 'supertest';
+import app from '@/app';
+import { faker } from '@faker-js/faker';
+import { documentation } from '../setup';
+import { DroneModel } from '@/database/enum';
+
+const server = supertest.agent(app);
+
+describe('Dispatcher Test E2E', () => {
+  describe('Register Drone', () => {
+    it('should register a drone', async () => {
+      const res = await server.post(`/drones`)
+        .send({
+          model: faker.helpers.enumValue(DroneModel),
+          serialNumber: faker.airline.flightNumber({ length: 100 })
+        });
+
+      if (res.error) console.log(res.error);
+
+      assert.equal(res.statusCode, 200);
+      documentation.addEndpoint(res, {
+        tags: ['Drone'],
+      });
+    });
+  });
+});
---
+++ test/setup.ts
@@ -0,0 +1,21 @@
+import { AppDataSource } from '@/database/connection';
+import { Documentation } from '@obisiket1/express-utils';
+import { join } from "path"
+
+export const documentation = new Documentation({
+  title: "Dispatcher",
+  filePaths: join(__dirname, '..', 'docs', 'api.json'),
+});
+
+export const mochaHooks = {
+  async beforeAll(): Promise<void> {
+    await AppDataSource.initialize();
+  },
+
+  async afterAll(): Promise<void> {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+    documentation.renderDocumentation();
+  },
+};
